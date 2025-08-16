b196cb9563a36917e9959a528e71830153c50895
GrCOTE7 <grcote7@gmail.com>
2025-08-16 18:36:04 +0200

    Deployed 6a8e8a7 with MkDocs version: 1.6.1

---
+++ xtra/javascripts/essai.js
@@ -0,0 +1,7 @@
+console.log('Oki')
+
+// document.querySelectorAll(".md-typeset details.warning").forEach((el) => {
+//   if (el.querySelector("summary")?.textContent.includes("ATTENTION")) {
+//     el.classList.add("unreleased-warning");
+//   }
+// });
