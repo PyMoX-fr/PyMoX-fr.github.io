497bfae2848623748943093c55f9e07ef10be2b8
GrCOTE7 <grcote7@gmail.com>
2026-01-28 13:58:42 +0100

    fix: type vars

--- docs/includes/sympy/scripts/construction_sandbox_graph_00.py
+++ docs/includes/sympy/scripts/construction_sandbox_graph_00.py
@@ -11,17 +11,19 @@ else:
 # --- PYODIDE:code --- #
 # Ceci est basique, mais ne t'emêche pas de t'amuser ad'libitum en le modifiant 😊 !
 
+
 def multiples(n):
-    """Les 10 premiers multiples de n
-    """
+    """Les 10 premiers multiples de n"""
     ms = []
     for i in range(11):
         ms.append(f"{i*n:>3}")
-    return f'- {n}:', *ms
+    return f"- {n}:", *ms
 
 
-vs = 2, 3  # Par exemple, ajoute ici juste ', 5'
-# print("Multiples de :")
-print('Multiples de :')
+vs: tuple[int, int] = (2, 3)
+# Par exemple, ajoute ici ainsi juste ', 5' :
+# vs: tuple[int, int, int] = (2, 3, 5)
+
+print("Multiples de :")
 for v in vs:
     print(*multiples(v))
